f04dbfe2b40dacd1748159992de6b63dad075c90
thais <thaisviana081@outlook.com>
2022-07-29 10:15:13 -0300

    configurando o redux ✔️

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,15 @@
-// Coloque aqui suas actions
+export const ADD_USER = 'ADD_USER';
+export const PERSONAL_INFO = 'PERSONAL_INFO';
+
+export const addingUsers = (emails) => (
+  {
+    type: 'ADD_USER',
+    emails,
+  }
+);
+
+export const personalInformation = (payload) => (
+  { type: 'PERSONAL_INFO',
+    payload,
+  }
+);
