d0cf8bc61afc4dd507a2d16744f5b939c33ede19
thais <thaisviana081@outlook.com>
2022-07-29 15:29:24 -0300

    1. Crie uma página inicial de login com os seguintes campos e características 🤯 ✓

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Header</div>
+        <Route path="/" component={ Login } />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,98 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { addingUsers } from '../redux/actions/index';
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      btnEnviar: true,
+    };
   }
+
+  handlerEmail= ({ target }) => {
+    /* console.log(target.value); */
+    const { value } = target;
+    const validationEmail = /\S+@\S+\.\S+/;
+    const valid = validationEmail.test(value);
+    if (valid === true) {
+      this.setState({
+        email: value,
+      });
+    } else {
+      this.setState({
+        btnEnviar: true,
+      });
+    }
+  }
+
+   handlerPassword= ({ target }) => {
+     /* console.log(target.value); */
+     const { value } = target;
+     const { email } = this.state;
+     const numValidPassword = 6;
+     if (value.length >= numValidPassword && email.length > numValidPassword) {
+       /* console.log(value); */
+       this.setState({
+         btnEnviar: false,
+       });
+     } else {
+       this.setState({
+         btnEnviar: true,
+       });
+     }
+   }
+
+   render() {
+     const { email, btnEnviar } = this.state;
+     const { loginSubmit } = this.props;
+
+     return (
+       <main>
+         <div>Login</div>
+         <label htmlFor="email-input">
+           E-mail:
+           <input
+             type="text"
+             data-testid="email-input"
+             id="email-input"
+             name="email"
+             onChange={ (e) => this.handlerEmail(e) }
+           />
+         </label>
+
+         <label htmlFor="password-input ">
+           Senha:
+           <input
+             type="text"
+             data-testid="password-input"
+             id="password-input "
+             name="password"
+             onChange={ (e) => this.handlerPassword(e) }
+           />
+         </label>
+
+         <Link to="/carteira">
+           <button
+             type="button"
+             disabled={ btnEnviar }
+             onClick={ () => loginSubmit(email) }
+           >
+             Entrar
+
+           </button>
+         </Link>
+       </main>);
+   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  loginSubmit: (param) => dispatch(addingUsers(param)) });
+
+Login.propTypes = {
+  loginSubmit: PropTypes.func.isRequired,
+};
+export default connect(null, mapDispatchToProps)(Login);
