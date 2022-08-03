594770ef0dba20e726f3683d9444788273767836
thais <thaisviana081@outlook.com>
2022-08-03 13:36:11 -0300

    6. Desenvolva uma tabela  🤗 ✔

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
@@ -9,9 +10,8 @@ class Wallet extends React.Component {
       <>
         <div>TrybeWallet</div>
         <Header />
-        {' '}
-
         <WalletForm />
+        <Table />
       </>
     );
   }
