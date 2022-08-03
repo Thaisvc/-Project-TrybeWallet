8c8a2e75f3e75bddcea1db51693ff4477bb5f48a
thais <thaisviana081@outlook.com>
2022-08-03 13:36:11 -0300

    6. Desenvolva uma tabela  🤗 ✔

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,35 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <table width="1000" align="center" border="1">
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+
+            <tr>
+              <td>otro</td>
+              <td>outo</td>
+              <td>outro</td>
+              <td>outro</td>
+              <td>outro</td>
+              <td>outro</td>
+              <td>outro</td>
+              <td>outro</td>
+              <td>outro</td>
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
