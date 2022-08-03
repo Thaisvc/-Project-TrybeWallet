21835654e1ecf4b0484cb0e5ecf612f6ec0102c9
thais <thaisviana081@outlook.com>
2022-08-03 16:23:11 -0300

    7. Implemente a lógica para que a tabela seja alimentada pelo estado da aplicação  ✔ 😅

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { stateInfo } = this.props;
     return (
-      <div>
-        <table width="1000" align="center" border="1">
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
-            </tr>
-
-            <tr>
-              <td>otro</td>
-              <td>outo</td>
-              <td>outro</td>
-              <td>outro</td>
-              <td>outro</td>
-              <td>outro</td>
-              <td>outro</td>
-              <td>outro</td>
-              <td>outro</td>
-            </tr>
-          </thead>
-        </table>
-      </div>
+      <table width="1000" align="center" border="1">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { stateInfo.map(({ id, description, tag, method,
+            value, currency, exchangeRates }) => {
+            const nunber = Number(value);
+            const exchangeRate = exchangeRates[currency];
+            return (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ nunber.toFixed(2) }</td>
+                <td>{ exchangeRate.name }</td>
+                <td>{ Number(exchangeRate.ask).toFixed(2) }</td>
+                <td>{ (exchangeRate.ask * value).toFixed(2) }</td>
+                <td>Real</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  // coin: state.wallet.currencies,
+  stateInfo: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  stateInfo: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
